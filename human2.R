2f356dfb6a8c9803421599eb7664255dd045c035
dmitanas <anastasija.dmitrijeva@helsinki.fi>
2021-11-30 12:24:15 +0200

    changes

--- human2.R
+++ human2.R
@@ -67,7 +67,3 @@ human__ <- human_[complete.cases(human_), ]
 str(human__)
 
 write.table(human__, file = "human2.txt", row.names = TRUE)
-
-# The data now have 155 observations and 8 variables. 
-# Save the human data in your data folder including the row names. 
-# You can overwrite your old ‘human’ data. (1 point)
